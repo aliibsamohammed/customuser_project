4e51f2d9ddd64b2524aae9dbc878185aa25f2c24
aliibsamohammed <ibsamohammed@gmail.com>
2021-09-25 22:03:46 +0300

    further configuring for heroku

--- customuser_project/wsgi.py
+++ customuser_project/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
-
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'customuser_project.settings')
 
 application = get_wsgi_application()
